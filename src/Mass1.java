80c47f37d58033404d4ab78c84f65e4ab4d40e99
IlichM <viruslm@mail.ru>
2021-04-15 22:16:25 +0300

    Second commit

--- src/Mass1.java
+++ src/Mass1.java
@@ -3,7 +3,7 @@ public class Mass1 {
         int[] nums = new int[10];
         nums[0] = 1;
         nums[5] = 5;
-        nums[10] = 15;
+
 
         for (int i=0; i<nums.length; i++) {
             System.out.println(nums[i]);
